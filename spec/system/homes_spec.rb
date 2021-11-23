4137b418c02d909c09254c6db329cf0ca072c7f2
snafkin2222 <bunbun5719>
2021-11-23 20:54:53 +0900

    first commit

---
+++ spec/system/homes_spec.rb
@@ -0,0 +1,14 @@
+# require 'rails_helper'
+
+# RSpec.describe 'Home', type: :system do
+#   it 'shows greeting' do
+#     # root_pathへアクセス
+#     visit root_path
+#     # ページ内に'Hello World!'が含まれているかを検証
+#     expect(page).to have_content 'Hello World!'
+#     # 'Help'文字列をクリック
+#     click_on 'Help'
+#     # ページ内に'This is the help page.'が含まれているかを検証
+#     expect(page).to have_content 'This is the help page.'
+#   end
+# end
